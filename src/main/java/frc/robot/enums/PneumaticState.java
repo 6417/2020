31669e109ed5008bd0efed53f83b47fb92edde91
AnonCoder <gianlaager@outlook.com>
2020-01-17 17:36:43 +0100

    anfang der Software für das Pneumatic Subsystem

---
+++ src/main/java/frc/robot/enums/PneumaticState.java
@@ -0,0 +1,5 @@
+package frc.robot.enums;
+
+public enum PneumaticState {
+    OFF, FORWARD, REVERSE
+}
--- src/main/java/frc/robot/subsystems/PneumaticSubsystem.java
+++ src/main/java/frc/robot/subsystems/PneumaticSubsystem.java
@@ -9,12 +9,16 @@ package frc.robot.subsystems;
 
 import java.util.logging.Level;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import ch.team6417.lib.utils.LatchedBoolean;
 import ch.team6417.lib.utils.LatchedBoolean.EdgeDetection;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotContainer;
+import frc.robot.enums.PneumaticState;
 import lombok.extern.java.Log;
 
 @Log
@@ -38,23 +42,48 @@ public class PneumaticSubsystem extends SubsystemBase {
 
   }
 
-  @Override
-  public void periodic() {
-    if(compressor.getCompressorNotConnectedFault()) {
-      log.log(Level.SEVERE, "Compressor not connected!");
-    }
-    if (compressor.getCompressorCurrentTooHighFault()) {
-      log.log(Level.SEVERE, "Compressor current too high!");
-    }
-    if(compressor.getCompressorShortedFault()) {
-      log.log(Level.SEVERE, "Compressor shorted!");
-    }
+  // @Override
+  // public void periodic() {
+  //   if(compressor.getCompressorNotConnectedFault()) {
+  //     log.log(Level.SEVERE, "Compressor not connected!");
+  //   }
+  //   if (compressor.getCompressorCurrentTooHighFault()) {
+  //     log.log(Level.SEVERE, "Compressor current too high!");
+  //   }
+  //   if(compressor.getCompressorShortedFault()) {
+  //     log.log(Level.SEVERE, "Compressor shorted!");
+  //   }
 
-    if(pressureTankFull.update(compressor.getPressureSwitchValue()))
-    {
-      log.log(Level.INFO, "Pressure Tank full!");
+  //   if(pressureTankFull.update(compressor.getPressureSwitchValue()))
+  //   {
+  //     log.log(Level.INFO, "Pressure Tank full!");
+  //   }
+  // }
+  
+  void set(DoubleSolenoid cylinder, PneumaticState state) {
+    switch (state) {
+      case OFF:
+        cylinder.set(Value.kOff);
+        break;
+      
+      case FORWARD:
+        cylinder.set(Value.kForward);
+        break;
+
+      case REVERSE:
+        cylinder.set(Value.kReverse);
+        break;
     }
   }
 
-  
+  boolean getRead(WPI_TalonSRX motorController, int port) throws Exception{
+    switch (port) {
+      case 1:
+        return motorController.isFwdLimitSwitchClosed() == 1;
+      case 0:
+        return motorController.isRevLimitSwitchClosed() == 1;
+      default:
+        throw new Exception("Port must be 1(forward) or 0(reverse) not " + String.valueOf(port));
+    }
+  }
 }
