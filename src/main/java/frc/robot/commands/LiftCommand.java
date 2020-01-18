1696f31b093886b5de2340c1b61e11a3e4701e0c
Gian <gianlaager114@gmail.com>
2020-01-18 13:44:52 +0100

    änderungen im Lift command

--- src/main/java/frc/robot/commands/LiftCommand.java
+++ src/main/java/frc/robot/commands/LiftCommand.java
@@ -34,12 +34,12 @@ public class LiftCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_subsystem.extendLift();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    m_subsystem.extendLift();
   }
 
   // Called once the command ends or is interrupted.
